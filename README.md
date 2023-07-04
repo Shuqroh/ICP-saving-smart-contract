f1bac814b17fb3ab28bb6351a012fedc251b729f
Shuqroh <92368644+Shuqroh@users.noreply.github.com>
2023-07-04 09:23:28 +0100

    added files
    Added all files and updated both readme and index.ts file

---
+++ README.md
@@ -0,0 +1,17 @@
+# ICP Saving Contract
+
+This smart contract, which was created using the Azle framework (Canister) Typescript on the ICP Blockchain, aims to let save into the smart contract at their discretion.
+
+`createSaving` : create new savings
+
+`getAllSavings` : get all savings
+
+`getSavingById` : get saving by id
+
+`calculateApy` : calculate apy
+
+`deleteSaving` : delete savings
+
+`fundSaving` : fund saving
+
+`withdrawSaving` : withdraw savings
---
+++ src/index.ts
@@ -0,0 +1,127 @@
+import {
+  $query,
+  $update,
+  Record,
+  StableBTreeMap,
+  Vec,
+  match,
+  Result,
+  nat64,
+  ic,
+  Opt,
+} from "azle";
+import { v4 as uuidv4 } from "uuid";
+
+type Saving = Record<{
+  id: string;
+  username: string;
+  specifiedYear: number;
+  amount: number;
+  noOfDays: number;
+  createdAt: nat64;
+  updatedAt: Opt<nat64>;
+}>;
+
+type SavingPayload = Record<{
+  username: string;
+  specifiedYear: number;
+  amount: number;
+  noOfDays: number;
+}>;
+
+const savingsStorage = new StableBTreeMap<string, Saving>(0, 44, 1024);
+
+$query;
+export function getAllSavings(): Result<Vec<Saving>, string> {
+  return Result.Ok(savingsStorage.values());
+}
+
+$update;
+export function createSaving(payload: SavingPayload): Result<Saving, string> {
+  const saving: Saving = {
+    id: uuidv4(),
+    createdAt: ic.time(),
+    updatedAt: Opt.None,
+    ...payload,
+  };
+  savingsStorage.insert(saving.id, saving);
+  return Result.Ok(saving);
+}
+
+$query;
+export function getSavingById(id: string): Result<Saving, string> {
+  return match(savingsStorage.get(id), {
+    Some: (saving) => Result.Ok<Saving, string>(saving),
+    None: () => Result.Err<Saving, string>(`saving with id=${id} not found`),
+  });
+}
+
+$query;
+export function calculateApy(
+  days: number,
+  amount: number,
+  specifiedyear: number
+): Result<number, string> {
+  const quoficientOfDays = days / specifiedyear;
+  return quoficientOfDays * amount;
+}
+
+$update;
+export function deleteSaving(id: string): Result<Saving, string> {
+  return match(savingsStorage.remove(id), {
+    Some: (saving) => Result.Ok<Saving, string>(saving),
+    None: () =>
+      Result.Err<Saving, string>(
+        `couldn't delete saving with id=${id}. Profile not found.`
+      ),
+  });
+}
+
+$update;
+export function fundSaving(id: string, amount: number): Result<Saving, string> {
+  const saving = getSavingById(id);
+
+  if (saving.isErr()) {
+    return Result.Err<Saving, string>(`saving with id=${id} not found.`);
+  }
+
+  const updatedSaving = {
+    ...saving.unwrap(),
+    amount: saving.unwrap().amount + amount,
+  };
+
+  savingsStorage.insert(saving.id, updatedSaving);
+
+  return Result.Ok<Saving, string>(updatedSaving);
+}
+
+$update;
+export function withdrawSaving(id: string): Result<Saving, string> {
+  const saving = getSavingById(id);
+
+  if (saving.isErr()) {
+    return Result.Err<Saving, string>(`saving with id=${id} not found.`);
+  }
+
+  const updatedSaving = {
+    ...saving.unwrap(),
+    amount: 0,
+  };
+
+  savingsStorage.insert(saving.id, updatedSaving);
+
+  return Result.Ok<Saving, string>(updatedSaving);
+}
+
+// a workaround to make uuid package work with Azle
+globalThis.crypto = {
+  getRandomValues: () => {
+    let array = new Uint8Array(32);
+
+    for (let i = 0; i < array.length; i++) {
+      array[i] = Math.floor(Math.random() * 256);
+    }
+
+    return array;
+  },
+};
